461240c92b7cb33c3be7f042d6e50bfaeb91aaf1
Noura Kaddoura <nourakaddoura@gmail.com>
2019-01-07 17:53:05 -0800

    Class

---
+++ Starter_Code/Relaxer_Solution/js/app.js
@@ -0,0 +1,8 @@
+ $(document).ready(function() {
+	console.log("ready")
+  $('header nav ul li:first-child').click(function(){
+    $(this).parent().toggleClass('slideDown');
+  });
+});
+
+
---
+++ Starter_Code/tic_tac_toe/js/index.js
@@ -0,0 +1,7 @@
+
+
+//we need document.ready to load the page
+
+//A tic-tac-toe game can have at most 9 moves. We'll maintain a variable for this and also a variable to check if the game is still going on and hasn't been won by any player yet. 
+
+//The logic for player moves is as follows: Each player goes by pressing on an empty space on the board and also while the game is still going on. We'll use the jQuery click function for this.
